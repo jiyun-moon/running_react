84ccfdeabf3e35902d81cebb2bef3edb7f39c143
likemoonji <jiyun0527@gmail.com>
2020-03-27 23:30:11 +0900

    5장 props 처리 방법

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,16 +2,26 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Hello from './hello'
+import Wrapper from './wrapper';
 
 function App() {
-  const name = "jiyun moon"
+  const name = "jiyun moon";
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1.2rem'
+  }
   return (
-    <div className="App">
-      <header>
+    <Wrapper>
+      {/* 주석 테스트 */}
+      <header style={style}>
         {name}의 러닝 리액트
       </header>
-      <Hello />
-    </div>
+      <Hello name={name} color="red"/>
+      <Hello color="pink"/>
+      <div className="gray-box">gray box</div>
+    </Wrapper>
   );
 }
 
--- begin-react/src/hello.js
+++ begin-react/src/hello.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-function hello() {
-  return <h1>안녕하세요.</h1>
+function hello({ color, name }) {
+return <h1 style={{ color: color }}>안녕하세요. {name}님</h1>
 }
 
+hello.defaultProps = {
+  name: '이름없음'
+}
 export default hello;
